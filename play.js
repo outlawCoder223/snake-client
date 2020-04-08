bada70a9e66b46cdbd9a8d84067a6164579f6c75
Rance Campbell <rancewcampbell@gmail.com>
2020-04-08 20:29:26 +0000

    copied setupInput into input.js file

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const { connect } = require('./client');
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  
-  handleUserInput();
-  return stdin;
-};
-
-const handleUserInput = () => {
-  process.stdin.on('data', (key) => {
-    console.log(key);
-    if(key === '\u0003') process.exit();
-  });
-}; 
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-setupInput();
 connect();
+
+setupInput();
