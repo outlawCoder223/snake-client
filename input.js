8ef6d33132add4ca66599c1bfa612261ceaed06e
Rance Campbell <rancewcampbell@gmail.com>
2020-04-08 21:51:59 +0000

    added full movement to game

--- input.js
+++ input.js
@@ -1,16 +1,22 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  
+  connection = conn;
   handleUserInput();
   return stdin;
 };
 
 const handleUserInput = () => {
   process.stdin.on('data', (key) => {
-    console.log(key);
+    if (key === 'w') connection.write('Move: up');
+    if (key === 'a') connection.write('Move: left');
+    if (key === 's') connection.write('Move: down');
+    if (key === 'd') connection.write('Move: right');
+
     if (key === '\u0003') process.exit();
   });
 };
