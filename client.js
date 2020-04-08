2db481683f837e767f3311dcf2927f2f66774da6
Rance Campbell <rancewcampbell@gmail.com>
2020-04-08 19:36:20 +0000

    hard coded movement into snake

--- client.js
+++ client.js
@@ -6,8 +6,10 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '50.64.116.162',
+    // host: '50.64.116.162',
+    host: 'localhost',
     port: 50541
+    
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
@@ -16,6 +18,19 @@ const connect = function() {
     console.log('Successfully connected to game server');
     conn.write('Name: RWC');
   });
+
+  conn.on('connect', () => {
+    setTimeout(() => {
+      conn.write('Move: up');
+    }, 1000);
+  });
+  conn.on('connect', () => {
+    setInterval(() => {
+      conn.write('Move: up');
+    }, 2000);
+  });
+
+  
 
   conn.on('data', (data) => {
     console.log(data);
